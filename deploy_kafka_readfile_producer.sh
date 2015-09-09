195dbd0c85f9a67f7ae6e8b09ac72ed5e922e3d6
Shane Kumpf <sakserv@sakserv.org>
2015-09-09 14:40:17 -0500

    add dev and prod cluster properties

--- deploy_kafka_readfile_producer.sh
+++ deploy_kafka_readfile_producer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+   ENV_NAME=sandbox
+else
+   ENV_NAME=$1
+fi
+
 # Clean up
 cd /tmp && rm -rf build-tools
 
@@ -7,7 +13,7 @@ cd /tmp && rm -rf build-tools
 git clone https://github.com/sakserv/build-tools.git
 
 # Grab the latest kafka-readfile-producer
-build-tools/nexus_artifact_download.sh -i "com.github.sakserv:kafka-readfile-producer:0.0.2" -p jar
+build-tools/nexus_artifact_download.sh -i "com.github.sakserv:kafka-readfile-producer:0.0.3" -p jar
 
 # Run the producer code
-java -cp "/tmp/kafka-readfile-producer-0.0.2.jar:/usr/hdp/current/kafka-broker/libs/kafka_2.10-0.8.2.2.3.0.0-2557.jar" com.github.sakserv.kafka.producer.KafkaReadfileProducerCli /tmp/build-tools/conf/sandbox.properties /tmp/build-tools/samples/kafka_readfile_producer_input.txt
+java -cp "/tmp/kafka-readfile-producer-0.0.3.jar:/usr/hdp/current/kafka-broker/libs/kafka_2.10-0.8.2.2.3.0.0-2557.jar" com.github.sakserv.kafka.producer.KafkaReadfileProducerCli /tmp/build-tools/conf/${ENV_NAME}.properties /tmp/build-tools/samples/kafka_readfile_producer_input.txt
