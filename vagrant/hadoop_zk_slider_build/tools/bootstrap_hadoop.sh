8c6426471e53804f5596d9b35844810c696e1791
Shane Kumpf <sakserv@sakserv.org>
2016-03-21 12:52:25 -0600

    properly stage the dist tarball

--- vagrant/hadoop_zk_slider_build/tools/bootstrap_hadoop.sh
+++ vagrant/hadoop_zk_slider_build/tools/bootstrap_hadoop.sh
@@ -20,6 +20,9 @@ cp -Rp $HADOOP_SRC_DIR/* $HADOOP_STG_DIR
 echo "#### Running the hadoop build"
 cd $HADOOP_STG_DIR && mvn clean install package -Pnative,dist -Dtar -Dcontainer-executor.conf.dir=../etc/hadoop -DskipTests -Dmaven.javadoc.skip=true
 
+echo "#### Staging the hadoop archive"
+cp $HADOOP_STG_DIR/hadoop-dist/target/hadoop-*.tar.gz /tmp/hadoop.tar.gz
+
 #
 # Installing Hadoop
 #
