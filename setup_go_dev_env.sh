60cec1db252a38a8d4a1d5fa5a405f175c13a825
Shane Kumpf <sakserv@sakserv.org>
2016-03-31 10:16:27 -0400

    add script to setup a go dev environment

---
+++ setup_go_dev_env.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+#
+# Variables
+#
+GOPATH=/go
+GOINSTPATH=/usr/local
+GOROOT=$GOINSTPATH/go
+GOTARBALLURL=https://storage.googleapis.com/golang/go1.6.linux-amd64.tar.gz
+GOTARBALLLOCAL=/tmp/go.tar.gz
+
+#
+# Setup
+#
+echo -e "\n#### Creating $GOPATH if needed"
+if [ ! -d $GOPATH ]; then
+  mkdir -p $GOPATH
+fi
+
+echo -e "\n#### Creating $GOINSTPATH if needed"
+if [ ! -d $GOINSTPATH ]; then
+  mkdir -p $GOINSTPATH
+fi
+
+#
+# Main
+#
+echo -e "\n#### Downloading the go release"
+wget $GOTARBALL -O $GOTARBALLLOCAL || exit 1
+
+echo -e "\n#### Extracting the go release to $GOINSTPATH"
+tar -C $GOINSTPATH -xzf $GOTARBALLLOCAL || exit 1
+
+echo -e "\n#### Adding GOROOT to profile"
+echo "export GOROOT=$GOROOT >>/etc/profile"
+
+echo -e "\n#### Adding GOPATH to profile"
+echo "export GOPATH=$GOPATH >>/etc/profile"
+
+echo -e "\n#### Adding GOROOT/bin to PATH"
+echo "export PATH=$GOROOT/bin >>/etc/profile"
+
+echo -e "\n#### Sourcing the updated profile"
+. /etc/profile
+
+echo -e "\n#### Installing go dev tooling (golint, godep, etc)"
+go get github.com/tools/godep github.com/golang/lint/golint 
+
+exit 0
