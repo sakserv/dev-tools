cc4773511fc134a971e8aca9888126b6506cf17d
Shane Kumpf <sakserv@sakserv.org>
2017-10-20 06:37:14 -0600

    remove unnecessary steps

--- vagrant/hadoop-build/tools/bootstrap_hadoop_from_release_tgz.sh
+++ vagrant/hadoop-build/tools/bootstrap_hadoop_from_release_tgz.sh
@@ -16,32 +16,6 @@ HADOOP_SRC_DIR=/hadoop_src
 HADOOP_STG_DIR=/hadoop_staging
 ANSIBLE_HADOOP_STG_DIR=/ansible-hadoop_staging
 
-#
-# Installing required build deps
-#
-echo "#### Remving cmake 2"
-yum remove cmake -y
-
-echo "#### Installing openssl, cmake3, and protbuf"
-yum install openssl-devel cmake3 protobuf-devel -y
-
-echo "#### Add cmake symlink"
-ln -s /usr/bin/cmake3 /usr/bin/cmake
-
-#
-# Building Hadoop
-#
-echo "#### Staging code to $HADOOP_STG_DIR"
-if [ -d $HADOOP_STG_DIR ]; then
-  rm -rf $HADOOP_STG_DIR
-fi
-mkdir -p $HADOOP_STG_DIR
-cp -Rp $HADOOP_SRC_DIR/* $HADOOP_STG_DIR
-
-echo "#### Running the hadoop build"
-cd $HADOOP_STG_DIR 
-mvn clean install -Pnative,dist -Dtar -Dcontainer-executor.conf.dir=../etc/hadoop -Dcontainer-executor.additional_cflags="-DDEBUG" -DskipTests -Dmaven.javadoc.skip=true || exit 1
-
 echo "#### Staging the hadoop archive"
 cp $RELEASE_TGZ /tmp/hadoop.tar.gz
 
