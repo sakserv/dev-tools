0634b47ff163998ac941e58a08ebc682698f943c
Shane Kumpf <sakserv@sakserv.org>
2017-10-30 11:55:32 -0600

    fix maven install

--- vagrant/hadoop-build/tools/bootstrap_common.sh
+++ vagrant/hadoop-build/tools/bootstrap_common.sh
@@ -31,14 +31,14 @@ echo "export JAVA_HOME=$JAVA_HOME" >>/etc/profile
 #
 echo -e "\n#### Installing Maven"
 cd /tmp
-wget -N http://apache.cs.utah.edu/maven/maven-3/3.5.0/binaries/apache-maven-3.5.0-bin.tar.gz
-tar -xzvf apache-maven-3.5.0-bin.tar.gz -C /usr/local/bin
-export M2_HOME=/usr/local/bin/apache-maven-3.5.0/
+wget -N http://mirror.stjschools.org/public/apache/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
+tar -xzvf apache-maven-3.5.2-bin.tar.gz -C /usr/local/bin
+export M2_HOME=/usr/local/bin/apache-maven-3.5.2/
 export M2=$M2_HOME/bin
 export JAVA_HOME=$JAVA_HOME
 export PATH=$PATH:$M2:$JAVA_HOME/bin
 echo "export PATH=$PATH:$M2:$JAVA_HOME/bin" >>/etc/profile
-. /etc/profile
+source /etc/profile
 mvn --version
 
 #
